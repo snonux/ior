6e03b744ba378c48b5120f7cf59f0b588a7cbd46
Paul Buetow <paul@buetow.org>
2024-02-04 10:53:54 +0200

    remove paul from paths

--- build.sh
+++ build.sh
@@ -2,11 +2,13 @@
 
 set -xeuf -o pipefail
 
+declare -r LIBBPFGO="$(pwd)/../libbpfgo"
+
 clang -g -O2 -Wall -fpie -I../libbpfgo/selftest/common -target bpf -D__TARGET_ARCH_amd64 -I../libbpfgo/output -I../libbpfgo/selftest/common -c main.bpf.c -o main.bpf.o
 
 export CC=clang
-export CGO_CFLAGS="-I/home/paul/git/libbpfgo/output -I/home/paul/git/libbpfgo/selftest/common"
-export CGO_LDFLAGS="-lelf -lzstd /home/paul/git/libbpfgo/output/libbpf.a"
+export CGO_CFLAGS="-I$LIBBPFGO/output -I$LIBBPFGO/selftest/common"
+export CGO_LDFLAGS="-lelf -lzstd $LIBBPFGO/output/libbpf.a"
 export GOOS=linux
 export GOARCH=amd64
 
