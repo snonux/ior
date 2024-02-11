5ed09a6a0254a65bb57618463eb3754402615160
Paul Buetow <paul@buetow.org>
2024-02-11 22:40:36 +0200

    also use opids.go

--- build.sh
+++ build.sh
@@ -5,9 +5,9 @@ set -xeuf -o pipefail
 declare -r LIBBPFGO="$(pwd)/../libbpfgo"
 
 bpftool btf dump file /sys/kernel/btf/vmlinux format c > vmlinux.h
-echo 'package main' > opids.go
-echo >> opids.go
-sed -E 's/#define (.*) ([0-9]+)/const \1 = \2/' opids.h >> opids.go
+echo 'package main' > internal/opids.go
+echo >> internal/opids.go
+sed -E 's/#define (.*) ([0-9]+)/const \1 = \2/' opids.h >> internal/opids.go
 
 clang -g -O2 -Wall -fpie -I../libbpfgo/selftest/common -target bpf -D__TARGET_ARCH_amd64 -I../libbpfgo/output -I../libbpfgo/selftest/common -c main.bpf.c -o main.bpf.o
 
