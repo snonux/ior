81c37a1c49b8fefd6aec62f1f53dce4965f96d34
Paul Buetow <paul@buetow.org>
2024-02-19 11:55:38 +0200

    add generated list, too

---
+++ internal/tracepoints/tracepoint_list.go
@@ -0,0 +1,13 @@
+// Auto-generated from C, don't change manually!
+package tracepoints
+
+var tracepointList = []string{
+	"enter_close",
+	"exit_close",
+	"enter_openat",
+	"exit_openat",
+	"enter_open",
+	"exit_open",
+	"enter_write",
+	"exit_write",
+}
